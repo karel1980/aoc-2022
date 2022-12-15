557b9080340b3c5b88b920e51f6437ca5d8161df
Karel Vervaeke <karel.vervaeke@conundra.eu>
2022-12-15 19:41:53 +0100

    avoid large mem allocations

--- day15.py
+++ day15.py
@@ -1,10 +1,66 @@
 import re
 
-lines = [ l.strip() for l in open('data/day15').readlines() ]
-#lines = [ l.strip() for l in open('data/day15.sample').readlines() ]
+#lines = [ l.strip() for l in open('data/day15').readlines() ]
+lines = [ l.strip() for l in open('data/day15.sample').readlines() ]
 
 
-def count_without_beacons(lines, target):
+def simplify_ranges(ranges):
+    ranges = list(sorted(ranges))
+
+    i = 0
+    while i < len(ranges)-1:
+        a = ranges[i]
+        b = ranges[i+1]
+        if b[0] <= a[1] + 1:
+            del ranges[i+1]
+            ranges[i] = (a[0], max(a[1],b[1]))
+
+    return ranges
+
+def count_without_beacons(sensors, target):
+    pattern = re.compile("Sensor at x=([-\d]*), y=([-\d]*): closest beacon is at x=([-\d]*), y=([-\d]*)")
+    ranges = set()
+    bxs = set()
+    for sensor in sensors:
+        sx = sensor[0][0]
+        sy = sensor[0][1]
+        bx = sensor[1][0]
+        by = sensor[1][1]
+
+        if by == target:
+            bxs.add(bx)
+
+        sensor_range = abs(by-sy) + abs(bx-sx)
+
+        distance_from_target = abs(target - sy)
+
+        if sensor_range < distance_from_target:
+            # sensor does not give information about target
+            continue
+        delta = sensor_range - distance_from_target
+        ranges.add((sx - delta, sx + delta))
+    
+    print("before simplification", list(sorted(ranges)))
+    ranges = simplify_ranges(ranges)
+    print("after simplification", list(sorted(ranges)))
+
+    not_sensored = 0
+    for r in ranges:
+        not_sensored += r[1] - r[0] + 1
+
+#    positions = set()
+#    for r in ranges:
+#        for i in range(r[0], r[1]+1):
+#            positions.add(i)
+#
+#    positions -= bxs
+    #print(bxs)
+    #print(list(sorted(positions)))
+
+    return not_sensored - len(set(bxs))
+
+def parse_lines(lines):
+    result = []
     pattern = re.compile("Sensor at x=([-\d]*), y=([-\d]*): closest beacon is at x=([-\d]*), y=([-\d]*)")
     ranges = set()
     bxs = set()
@@ -19,33 +75,10 @@ def count_without_beacons(lines, target):
         sy = int(m.groups()[1])
         bx = int(m.groups()[2])
         by = int(m.groups()[3])
+        result.append(((sx,sy),(bx,by)))
 
-        if by == target:
-            bxs.add(bx)
-
-        #print(sx, sy, bx, by)
-
-        sensor_range = abs(by-sy) + abs(bx-sx)
-
-        distance_from_target = abs(target - sy)
-
-        if sensor_range < distance_from_target:
-            # sensor does not give information about target
-            continue
-        delta = sensor_range - distance_from_target
-        ranges.add((sx - delta, sx + delta))
+    return result
     
-
-    positions = set()
-    for r in ranges:
-        for i in range(r[0], r[1]+1):
-            positions.add(i)
-
-    positions -= bxs
-    #print(bxs)
-    #print(list(sorted(positions)))
-
-    return len(positions)
-
-print("number of non-beacons at line 10:", count_without_beacons(lines, 10))
-print("number of non-beacons at line 2M:", count_without_beacons(lines, 2000000))
+sensors = parse_lines(lines)
+print("number of non-beacons at line 10:", count_without_beacons(sensors, 10))
+print("number of non-beacons at line 2M:", count_without_beacons(sensors, 2000000))
