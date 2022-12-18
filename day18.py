47bfd644245fad006c3247099708c006ee90dcc4
Karel Vervaeke <karel.vervaeke@conundra.eu>
2022-12-18 12:45:52 +0100

    day18 part 2
    todo: is there a solution that would scale better?

--- day18.py
+++ day18.py
@@ -38,6 +38,18 @@ def count_trapped(voxels):
     return len(trapped)
 
 
+def count_faces2(v, voxels, outside):
+    x,y,z = v
+    result = 0
+    for d in [(0,0,1),(0,0,-1),(0,1,0),(0,-1,0),(1,0,0),(-1,0,0)]:
+        dx,dy,dz = d
+        w = (x+dx, y+dy, z+dz)
+        is_rock = w in voxels
+        if not is_rock and w in outside:
+            result += 1
+
+    return result
+
 def count_faces(v, voxels):
     x,y,z = v
     result = 0
@@ -53,11 +65,28 @@ def count_faces(v, voxels):
 def count_all_faces(voxels):
     return sum([count_faces(v, voxels) for v in voxels])
       
-faces = count_all_faces(voxels)
-trapped =count_trapped(voxels)
-print("faces", faces)
-print("trapped", trapped)
+def count_all_faces2(voxels, outside):
+    return sum([count_faces2(v, voxels, outside) for v in voxels])
+      
+def find_outside_air(voxels):
+    minx, miny, minz = min([v[0] for v in voxels]) -1, min([v[1] for v in voxels])-1, min([v[2] for v in voxels])-1
+    maxx, maxy, maxz = max([v[0] for v in voxels]) +1, max([v[1] for v in voxels])+1, max([v[2] for v in voxels])+1
+
+    origin = (minx, miny, minz)
+
+    outside=set()
+    queue=[origin]
+
+    while len(queue) > 0:
+        head = queue.pop()
+        outside.add(head)
+        for n in neighbors(head):
+            if n not in queue and n not in outside and minx<=n[0]<=maxx and miny<=n[1]<=maxy and minz<=n[2]<=maxz and n not in voxels:
+                queue.append(n)
+
+    return outside
 
 
-print(faces - 6 * trapped)
+outside = find_outside_air(voxels)
+print(count_all_faces2(voxels, outside))
 
